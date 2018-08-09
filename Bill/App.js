d46afefc345a5ac31b9f5fa42ba738d607463b56
Roscoe Coney <scoe@Roscoes-MBP.fios-router.home>
2018-08-09 13:59:16 -0400

    inital product with readme instruction

--- Bill/App.js
+++ Bill/App.js
@@ -1,13 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,29 +13,47 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Bill</Text>
+        <Text style={styles.instructions}>Dividing the final bill has NEVER been easier.</Text>
+        <Image style={styles.qrCode} source={require('./img/qrCode2.png')}/>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#252326',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 60,
     textAlign: 'center',
     margin: 10,
+    color: '#7a8c3f'
   },
   instructions: {
+        marginTop: 40,
     textAlign: 'center',
-    color: '#333333',
+    color: '#7a8c3f',
+    fontSize: 15,
     marginBottom: 5,
   },
+  qrCode: {
+    marginTop: 80,
+    width: 150,
+    height: 150 
+  }
 });
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Bill
 Application that seamlessly divides the check amongst friends.
+
+#Requirements
+1.  Node -v (8.3 or greater) - (brew install node)
+2. Watchman (brew install watchman)
+3. React Native command line interface (npm install -g react-native-cli)
+4. Xcode. (https://developer.apple.com/xcode/)
+
+
+#Installation Simulator Instructions
+1. (terminal) git clone https://github.com/BillsplitPAY/Bill.git
+2. (terminal) cd Bill
+3. (terminal) react-native run-ios
